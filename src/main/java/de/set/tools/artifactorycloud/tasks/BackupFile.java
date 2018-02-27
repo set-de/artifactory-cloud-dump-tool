d6c0958fd3986f36b25f98633c52b7f72345c80b
Niklas Walter <walter.niklas@gmail.com>
2018-02-27 09:21:50 +0100

    Validate file integrity after download

--- src/main/java/de/set/tools/artifactorycloud/tasks/BackupFile.java
+++ src/main/java/de/set/tools/artifactorycloud/tasks/BackupFile.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.attribute.FileTime;
 import java.util.concurrent.RecursiveAction;
 
 import org.jfrog.artifactory.client.Artifactory;
@@ -11,6 +12,9 @@ import org.jfrog.artifactory.client.RepositoryHandle;
 import org.jfrog.artifactory.client.model.LightweightRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.common.hash.HashCode;
+import com.google.common.hash.Hashing;
 
 import de.set.tools.artifactorycloud.model.FileListEntry;
 
@@ -38,14 +42,15 @@ public class BackupFile extends RecursiveAction {
     @SuppressWarnings("nls")
     @Override
     protected void compute() {
-        try {
-            LOG.info("Downloading {} in repository {}", this.file.getUri(), this.repository.getKey());
-            final RepositoryHandle handle = this.artifactory.repository(this.repository.getKey());
+        LOG.info("Downloading {} in repository {}", this.file.getUri(), this.repository.getKey());
+        final RepositoryHandle handle = this.artifactory.repository(this.repository.getKey());
+        try (InputStream input = handle.download(this.file.getUri()).doDownload()) {
+
             final Path target = this.repoDir.resolve("." + this.file.getUri());
             Files.createDirectories(target.getParent());
-            try (InputStream input = handle.download(this.file.getUri()).doDownload()) {
-                Files.copy(input, target);
-            }
+            Files.copy(input, target);
+            Files.setLastModifiedTime(target, FileTime.from(this.file.getLastModified().toInstant()));
+            this.verify(target);
 
         } catch (final IOException e) {
             throw new RuntimeException(String.format("Cannot download file %s of repository",
@@ -53,6 +58,27 @@ public class BackupFile extends RecursiveAction {
                     this.repository.getKey()), e);
         }
 
+    }
+
+    @SuppressWarnings("nls")
+    private void verify(final Path target) throws IOException {
+        final long fileSize = Files.size(target);
+        if (fileSize != this.file.getSize()) {
+            throw new IOException(
+                String.format("Size mismatch: expected %d but was %d %s", this.file.getSize(), fileSize, target));
+        }
+
+        final HashCode sha1 = this.sha1(target);
+        if (!HashCode.fromString(this.file.getSha1()).equals(sha1)) {
+             throw new IOException(
+                String.format("Sha-1 mismatch: expected %s but was %s %s", this.file.getSha1(), sha1, target));
+        }
+
+    }
+
+    @SuppressWarnings("deprecation")
+    private HashCode sha1(final Path file) throws IOException {
+        return com.google.common.io.Files.asByteSource(file.toFile()).hash(Hashing.sha1());
     }
 
 }
